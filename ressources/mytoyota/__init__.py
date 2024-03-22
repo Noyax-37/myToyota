c86fc20563fe813129c4b1099adba0c2f5a430c0
Noyax37 <alain.concours@free.fr>
2024-03-22 14:18:25 +0100

    béta 14
    suppression du python

--- ressources/mytoyota/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Toyota Connected Services Client."""
-#from importlib_metadata import version
-
-from mytoyota.client import MyT  # noqa
-
-#__version__ = version(__name__)
--- ressources/tests/unit_tests/test_init.py
+++
@@ -1,7 +0,0 @@
-"""pytest tests for mytoyota.__init__."""
-from mytoyota import MyT
-
-
-def test_imports():
-    """Ensure the imported module is the expected one."""
-    assert MyT is not None
